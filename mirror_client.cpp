ffc16e3031f9ca85b1e6c34b69f9f16b473b13a5
Fabian Rodriguez <farodriguez.49@gmail.com>
2015-09-01 18:19:52 -0600

    Primer commit

---
+++ mirror_client.cpp
@@ -0,0 +1,25 @@
+/**
+  * Universidad de Costa Rica
+  * Escuela de Ciencias de la Computacion e Informatica
+  * Laboratorio de Redes de Computadoras
+  * Laboratorio 4: Sockets, cliente y servidor
+  * @author Fabian Rodriguez Obando
+  * B25695
+  * II Semestre 2015
+  */
+
+#include "Socket.h"
+#include <iostream>
+
+using namespace std;
+int main(int argc, char *argv[ ]){
+
+    Socket s;
+    char buffer[512];
+
+    s.Connect( argv[2], 9876 ); // Same port as server
+    s.Write( argv[1], 80 );		// Send first program argument to server
+    s.Read( buffer, 512 );		// Read the answer sent back from server
+    printf( "%s \n", buffer );	// Print the string
+
+}
